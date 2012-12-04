86425650ab7b02597bbb9cd0a81a208700531e21
Evgeny Pavlov <lucenticus@gmail.com>
2012-12-04 12:42:12 +0400

    Added copyright

--- parser-afs.c
+++ parser-afs.c
@@ -1,3 +1,26 @@
+/*
+ * parser-afs.c - Parser from AFS to Recursive EQuations System
+ *
+ * Copyright (C) 2012 Evgeny Pavlov <lucenticus@gmail.com>
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * This file is part of AFS2REQS.
+ *
+ * AFS2REQS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * AFS2REQS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with AFS2REQS.  If not, see <http://www.gnu.org/licenses/>.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
 #include <stdlib.h>
 int column = 0;
 
